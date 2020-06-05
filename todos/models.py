61982c1d24e7f5fe1ecd5a97253d7918a3f3ea20
Jisun Park <fpdldk912@gmail.com>
2020-06-05 17:14:18 +0900

    add models and views

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Todo(models.Model):
+    todo = models.CharField(max_length=100)
+    isFinished = models.BooleanField(default=False)
+    untilWhen = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['untilWhen']
+
+    def __str__(self):
+        return self.todo
--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import status 
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Todo
+from .serializers import TodoSerializer
 
-# Create your views here.
+
+@api_view(['GET', 'POST'])
+def todo_list(request):
+    """
+    List all todos, or create new snippet.
+    """
+    if request.method == 'GET':
+        todos = Todo.objects.all()
+        serializer = TodoSerializer(todos, many=True)
+        return Response(serializer.data)
+    
+    elif request.method == 'POST':
+        serializer = TodoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
